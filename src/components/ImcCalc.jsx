ef073e2a1a6804080712202c696316dd4e08b48b
CoderLuizF <zzzezizzz@gmail.com>
2025-07-03 15:48:01 -0300

    feat: add form elements for BMI calculation

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const ImcCalc = () => {
   return (
-    <div>
+    <div id='calc-container'>
         <h2>Calculadora de IMC</h2>
+        <form id='imc-form'>
+            <div className="form-input">
+                <div className="form-control">
+                    <label htmlFor="height">Altura:</label>
+                    <input type="text" name='height' id='height' placeholder='Exemplo: 1,80' />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="weight">Peso:</label>
+                    <input type="text" name='weight' id='weight' placeholder='Exemplo: 80,5' />
+                </div>
+            </div>
+        </form>
     </div>
   )
 }
