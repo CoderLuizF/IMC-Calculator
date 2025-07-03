f06918f5a4dcbbe81c39090da97fb562a2b6ed1d
CoderLuizF <zzzezizzz@gmail.com>
2025-07-03 15:50:56 -0300

    feat: add buttons to BMI calculation form

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const ImcCalc = () => {
   return (
-    <div id='calc-container'>
-        <h2>Calculadora de IMC</h2>
-        <form id='imc-form'>
-            <div className="form-input">
-                <div className="form-control">
-                    <label htmlFor="height">Altura:</label>
-                    <input type="text" name='height' id='height' placeholder='Exemplo: 1,80' />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="weight">Peso:</label>
-                    <input type="text" name='weight' id='weight' placeholder='Exemplo: 80,5' />
-                </div>
-            </div>
-        </form>
+    <div id="calc-container">
+      <h2>Calculadora de IMC</h2>
+      <form id="imc-form">
+        <div className="form-input">
+          <div className="form-control">
+            <label htmlFor="height">Altura:</label>
+            <input
+              type="text"
+              name="height"
+              id="height"
+              placeholder="Exemplo: 1,80"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="weight">Peso:</label>
+            <input
+              type="text"
+              name="weight"
+              id="weight"
+              placeholder="Exemplo: 80,5"
+            />
+          </div>
+        </div>
+        <div className="action-control">
+            <button>Calcular</button>
+            <button>Limpar</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ImcCalc
+export default ImcCalc;
