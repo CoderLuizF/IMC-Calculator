2b8dcec83c9e08873b9a570767cf7124b3c50743
CoderLuizF <zzzezizzz@gmail.com>
2025-07-07 12:29:10 -0300

    feat: create BMI information table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { data } from "./data/data";
+import { useState } from "react";
+
 import ImcCalc from "./components/ImcCalc";
+import ImcTable from "./components/imcTable";
 import "./App.css";
 
 function App() {
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+    console.log("Executou");
+  };
+
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
   return (
     <>
       <div className="container">
-        <ImcCalc />
+        {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable />}
       </div>
     </>
   );
